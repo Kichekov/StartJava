907a2ab55402abb342d7a803fa6800f84707f459
Kichekov Vlad <kichekov@inbox.lv.>
2025-04-09 16:36:08 +0300

    Calculator var2

--- Calculator.java
+++ Calculator.java
@@ -4,16 +4,16 @@ class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
-            Для нахождения суммы введите: +
-            Для нахождения разности введите: -
-            Для нахождения произведения введите: *
-            для нахождения частного введите: /
-            Для возведения в степень введите: ^ 
-            Для деления по модулю введите: %"""
+                Для нахождения суммы введите: +
+                Для нахождения разности введите: -
+                Для нахождения произведения введите: *
+                для нахождения частного введите: /
+                Для возведения в степень введите: ^ 
+                Для деления по модулю введите: %"""
         );
         char sign = scanner.next().charAt(0);
         if (sign != '+' && sign != '-' && sign != '*' &&
-                 sign != '/' && sign != '^' && sign != '%') {
+                sign != '/' && sign != '^' && sign != '%') {
             System.out.println("Ошибка: Данная операция не предусмотрена");
             return;
         }
@@ -23,28 +23,8 @@ class Calculator {
         int base = 0;
         int degree = 0;
         if (sign != '^') {
-            System.out.println("Введите первый операнд: ");
-            if (scanner.hasNextInt()) {
-                firstOperand = scanner.nextInt();
-                if (firstOperand < 1) {
-                    System.out.println("Ошибка: Операнд не является натуральным числом");
-                    return;
-                }
-            } else {
-                System.out.println("Ошибка: Операнд не является натуральным числом");
-                return;
-            }
-            System.out.println("Введите второй операнд: ");
-            if (scanner.hasNextInt()) {
-                secondOperand = scanner.nextInt();
-                if (secondOperand < 1) {
-                    System.out.println("Ошибка: Операнд не является натуральным числом");
-                    return;
-                }
-            } else {
-                System.out.println("Ошибка: Операнд не является натуральным числом");
-                return;
-            }
+            firstOperand = setValue(scanner, "первый");
+            secondOperand = setValue(scanner, "второй");
             if (sign == '+') {
                 result = firstOperand + secondOperand;
             } else if (sign == '-') {
@@ -57,17 +37,47 @@ class Calculator {
                 result = firstOperand % secondOperand;
             }
         } else {
-            System.out.println("Введите основание степени: ");
-            base = scanner.nextInt();
-            System.out.println("Введите степень: ");
-            degree = scanner.nextInt();
+            base = setDegree(scanner, "Введите основание степени: ");
+            degree = setDegree(scanner, "Введите степень: ");
             result = base;
             for (int i = 1; i < degree; i++) {
                 result *= base;
             }
         }
-        System.out.printf("%d %c %d = %d \n", 
-                sign != '^' ? firstOperand : base, sign, 
+        System.out.printf("%d %c %d = %d \n",
+                sign != '^' ? firstOperand : base, sign,
                 sign != '^' ? secondOperand : degree, result);
+    }
+
+    public static int setValue(Scanner scanner, String operandNumber) {
+        System.out.printf("Введите %s операнд: ", operandNumber);
+        int operand = 0;
+        if (scanner.hasNextInt()) {
+            operand = scanner.nextInt();
+            if (operand < 1) {
+                System.out.println("Ошибка: Операнд не является натуральным числом");
+                System.exit(1); ;
+            }
+        } else {
+            System.out.println("Ошибка: Операнд не является натуральным числом");
+            System.exit(1); ;
+        }
+        return operand;
+    }
+
+    public static int setDegree(Scanner scanner, String operandNumber) {
+        System.out.printf("%s", operandNumber);
+        int operand = 0;
+        if (scanner.hasNextInt()) {
+            operand = scanner.nextInt();
+            if (operand < 1) {
+                System.out.println("Ошибка: Операнд не является натуральным числом");
+                System.exit(1); ;
+            }
+        } else {
+            System.out.println("Ошибка: Операнд не является натуральным числом");
+            System.exit(1); ;
+        }
+        return operand;
     }
 }
