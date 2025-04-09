9a36c12829b9c254ee79f35fb3663653c24abc1d
Kichekov Vlad <kichekov@inbox.lv.>
2025-04-09 16:43:31 +0300

    GuessNumber var2

--- GuessNumber.java
+++ GuessNumber.java
@@ -6,24 +6,23 @@ class GuessNumber {
         Random random = new Random();
         Scanner scanner = new Scanner(System.in);
         System.out.println("""
-            Добро пожаловать в игру УГАДАЙ ЧИСЛО!!!
-            -------------------------------------------
-            Правила игры: Компьютер "загадывает" целое число в отрезке [1-100].
-                        Вы должны его отгадать!
+                Добро пожаловать в игру УГАДАЙ ЧИСЛО!!!
+                -------------------------------------------
+                Правила игры: Компьютер "загадывает" целое число в отрезке [1-100].
+                              Вы должны его отгадать!
                       
-            После каждой неудачной попытки Вы получите подсказку.
-            Игра продолжится до тех пор, пока Вы не отгадаете загаданное
-            компьютером число!
-                    """);
+                После каждой неудачной попытки Вы получите подсказку.
+                Игра продолжится до тех пор, пока Вы не отгадаете загаданное
+                компьютером число!
+                        """);
         int randomNumber = random.nextInt(100) + 1;
-        boolean isVictory = false;
         int attemptsCounter = 1;
-        int userInput = 1;
+        int playerInput = 1;
         do {
             System.out.print("Введите число: ");
             if (scanner.hasNextInt()) {
-                userInput = scanner.nextInt();
-                if (userInput < 1 || userInput > 100) {
+                playerInput = scanner.nextInt();
+                if (playerInput < 1 || playerInput > 100) {
                     System.out.println("Ошибка: введенное Вами число не принадлежит" + 
                             " заданному отрезку, повторите попытку.");
                     continue;
@@ -32,15 +31,14 @@ class GuessNumber {
                 System.out.println("Ошибка: введенные Вами данные не являются натуральным числом.");
                 return;
             }
-            if (randomNumber == userInput) {
-                System.out.printf("Вы победили! Победной стала попытка № %s\n", attemptsCounter);
-                isVictory = true;
-            } else {
-                String message = userInput > randomNumber ? 
-                        "больше того, что загадал компьютер" : "меньше того, что загадал компьютер";
-                System.out.printf("Подсказка: %s %s\n", userInput, message);
+            if (randomNumber != playerInput) {
+                String message = playerInput > randomNumber ? 
+                        "больше" : "меньше";
+                System.out.printf("Подсказка: %s %s того, что загадал компьютер\n", 
+                        playerInput, message);
                 attemptsCounter++;
             }
-        } while (!isVictory);
+        } while (randomNumber != playerInput);
+        System.out.printf("Вы победили! Победной стала попытка № %s\n", attemptsCounter);
     }
 }
