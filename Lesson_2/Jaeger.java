0dd4c2b367a63ffc3f37e981445c7f688520266f
Kichekov Vlad <kichekov@inbox.lv.>
2025-04-24 23:58:51 +0300

    Lesson 2, class JaegerTest/Jaeger var1

---
+++ Lesson_2/Jaeger.java
@@ -0,0 +1,110 @@
+public class Jaeger {
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public Jaeger() {
+    }
+
+    public Jaeger(String modelName, String mark, String origin, float height, float weight, 
+            int speed, int strength, int armor) {
+        this.modelName = modelName;
+        this.mark = mark;
+        this.origin = origin;
+        this.height = height;
+        this.weight = weight;
+        this.speed = speed;
+        this.strength = strength;
+        this.armor = armor;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public void setMark(String mark) {
+        this.mark = mark;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public void setHeight(float height) {
+        this.height = height;
+    }
+
+    public void setWeight(float weight) {
+        this.weight = weight;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public void setArmor(int armor) {
+        this.armor = armor;
+    }
+
+    public String drift() {
+        return "Режим дрифт активирован";
+    }
+
+    public String move() {
+        return "Проверка систем движения, произвожу тесты";
+    }
+
+    public String scanKaiju() {
+        return "Произвожу сканирование Кайдзю";
+    }
+
+    public String useImpactGain() {
+        return "Гидравлическое усиление систем движения активировано, произвожу тесты";
+    }
+
+    public String useMachineGuns() {
+        return "Осуществляется тестовая стрельба по мишеням";
+    }
+
+    @Override
+     public String toString() {
+        return """
+            Название модели: %s
+            Семейство: %s
+            Происхождение: %s
+            Высота: %.2f м.
+            Вес: %.3f т.
+            Скорость: %d
+            Сила: %d
+            Броня: %d
+            Дрифт: %s
+            Движение: %s
+            Сканер: %s
+            Системы усиления: %s
+            Пулеметы: %s
+                """.formatted(
+                    modelName, 
+                    mark, 
+                    origin, 
+                    height, 
+                    weight, 
+                    speed,
+                    strength,
+                    armor,
+                    drift(),
+                    move(),
+                    scanKaiju(),
+                    useImpactGain(),
+                    useMachineGuns()
+                    );
+    }
+}
---
+++ Lesson_2/JaegerTest.java
@@ -0,0 +1,17 @@
+public class JaegerTest {
+    public static void main(String[] args) {
+        Jaeger chernoAlpha = new Jaeger();
+        chernoAlpha.setModelName("Cherno Alpha");
+        chernoAlpha.setMark("Mark-1");
+        chernoAlpha.setOrigin("Russia");
+        chernoAlpha.setHeight(85.34f);
+        chernoAlpha.setWeight(2.412f);
+        chernoAlpha.setSpeed(3);
+        chernoAlpha.setStrength(10);
+        chernoAlpha.setArmor(10);
+        System.out.println(chernoAlpha);
+
+        Jaeger romeoBlue = new Jaeger("Romeo Blue", "Mark-1", "USA", 77.72f, 7.775f, 2, 7, 6);
+        System.out.println(romeoBlue);
+    }
+}
