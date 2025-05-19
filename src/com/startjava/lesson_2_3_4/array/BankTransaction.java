0546d7e18a1cdc455fd37e2405ef84206483eec0
Kichekov Vlad <kichekov@inbox.lv.>
2025-05-19 19:54:39 +0300

    Lesson 4.BankTransaction var2

--- src/com/startjava/lesson_2_3_4/array/BankTransaction.java
+++ src/com/startjava/lesson_2_3_4/array/BankTransaction.java
@@ -4,54 +4,57 @@ import java.util.Arrays;
 
 public class BankTransaction {
     public static void main(String[] args) {
-        int[] originTransactionsId01 = {6, 8, 9, 1};
-        handleTransactions(originTransactionsId01);
+        int[] original1 = {6, 8, 9, 1};
+        int[] reversed1 = reverseTransactions(original1);
+        printTransactions(original1, reversed1);
 
-        int[] originTransactionsId02 = {13, 8, 5, 3, 2, 1, 1};
-        handleTransactions(originTransactionsId02);
+        int[] original2 = {13, 8, 5, 3, 2, 1, 1};
+        int[] reversed2 = reverseTransactions(original2);
+        printTransactions(original2, reversed2);
 
-        int[] originTransactionsId03 = {5};
-        handleTransactions(originTransactionsId03);
+        int[] original3 = {5};
+        int[] reversed3 = reverseTransactions(original3);
+        printTransactions(original3, reversed3);
 
-        int[] originTransactionsId04 = {};
-        handleTransactions(originTransactionsId04);
+        int[] original4 = {};
+        int[] reversed4 = reverseTransactions(original4);
+        printTransactions(original4, reversed4);
 
-        int[] originTransactionsId05 = null;
-        handleTransactions(originTransactionsId05);
+        int[] original5 = null;
+        int[] reversed5 = reverseTransactions(original5);
+        printTransactions(original5, reversed5);
     }
 
-    private static void handleTransactions(int[] originTransactions) {
+    private static void validateTransactions(int[] original) {
+        if (original == null) {
+            throw new IllegalArgumentException("Исходные транзакции: Ошибка ввода данных\n");
+        }
+        if (original.length == 0) {
+            throw new IllegalArgumentException("Исходные транзакции: Очередь транзакций пуста, " +
+                    "выполните хотя бы одну транзакцию\n");
+        }
+    }
+
+    private static int[] reverseTransactions(int[] original) {
+        if (original == null) {
+            return null;
+        }
+        int[] reversed = new int[original.length];
+        int length = original.length - 1;
+        for (int transaction : original) {
+            reversed[length] = transaction;
+            length--;
+        }
+        return reversed;
+    }
+
+    private static void printTransactions(int[] original, int[] reversed) {
         try {
-            validateTransactions(originTransactions);
-            int[] reverseTransactions = reverseTransactions(originTransactions);
-            printTransactions(originTransactions, reverseTransactions);
+            validateTransactions(original);
+            System.out.println("Исходные транзакции: " + Arrays.toString(original) + "\n" +
+                    " В обратном порядке: " + Arrays.toString(reversed) + "\n");
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
         }
-    }
-
-    private static void validateTransactions(int[] originTransactions) {
-        if (originTransactions == null) {
-            throw new IllegalArgumentException("Исходные транзакции: Ошибка ввода данных");
-        }
-        if (originTransactions.length == 0) {
-            throw new IllegalArgumentException("Исходные транзакции: Очередь транзакций пуста, " +
-                    "выполните хотя бы одну транзакцию");
-        }
-    }
-
-    private static int[] reverseTransactions(int[] originTransactions) {
-        int[] reverseTransactions = new int[originTransactions.length];
-        int length = originTransactions.length - 1;
-        for (int i = 0; i < originTransactions.length; i++) {
-            reverseTransactions[length] = originTransactions[i];
-            length--;
-        }
-        return reverseTransactions;
-    }
-
-    private static void printTransactions(int[] originTransactions, int[] reverseTransactions) {
-        System.out.println("Исходные транзакции: " + Arrays.toString(originTransactions) + "\n" +
-                " В обратном порядке: " + Arrays.toString(reverseTransactions) + "\n");
     }
 }
