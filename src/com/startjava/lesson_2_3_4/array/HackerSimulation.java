0f4368356fa2839e9b73710b513f1676652ab1ff
Kichekov Vlad <kichekov@inbox.lv.>
2025-05-22 22:27:44 +0300

    Lesson4 HackerSimulation var2

--- src/com/startjava/lesson_2_3_4/array/HackerSimulation.java
+++ src/com/startjava/lesson_2_3_4/array/HackerSimulation.java
@@ -4,27 +4,23 @@ public class HackerSimulation {
     private static final char[] SPINNER = {'-', '\\', '|', '/'};
     public static final String RED = "\u001B[31m";
     public static final String GREEN = "\u001B[32m";
+    private static int randomNumber;
 
     public static void main(String[] args) throws InterruptedException {
         hack();
-        int randomNumber = assignRandomNumber();
-        printHackResult(randomNumber);
+        printHackResult();
     }
 
     private static void hack() throws InterruptedException {
-        for (int i = 0, totalItem = 13; i < totalItem; i++) {
-            int index = i % 4;
-            char item = SPINNER[index];
+        randomNumber = (int) (Math.random() * 100);
+        for (int i = 0; i < 13; i++) {
+            char item = SPINNER[i % 4];
             System.out.print("\rHacking: " + item);
             Thread.sleep(400);
         }
     }
 
-    private static int assignRandomNumber() {
-        return (int) (Math.random() * 100);
-    }
-
-    private static void printHackResult(int randomNumber) {
+    private static void printHackResult() {
         System.out.printf("\rHacking: %s ", randomNumber > 70 ?
                 GREEN + "Access Granted!" : RED + "Access Denied!");
     }
