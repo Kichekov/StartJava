ae537a5377a26d75d27e47bc3b40953974435d5e
Kichekov Vlad <kichekov@inbox.lv.>
2025-05-23 23:51:00 +0300

    Lesson4 FactorialCalculator var2

--- src/com/startjava/lesson_2_3_4/array/FactorialCalculator.java
+++ src/com/startjava/lesson_2_3_4/array/FactorialCalculator.java
@@ -2,25 +2,25 @@ package com.startjava.lesson_2_3_4.array;
 
 public class FactorialCalculator {
     public static void main(String[] args) {
-        int[] numbers1 = {};
-        long[] factorials1 = calculateFactorial(numbers1);
-        printFactorial(numbers1, factorials1);
+        int[] numbers = {};
+        long[] factorials = calculateFactorial(numbers);
+        printFactorialsExpr(numbers, factorials);
 
-        int[] numbers2 = null;
-        long[] factorials2 = calculateFactorial(numbers2);
-        printFactorial(numbers2, factorials2);
+        numbers = null;
+        factorials = calculateFactorial(numbers);
+        printFactorialsExpr(numbers, factorials);
 
-        int[] numbers3 = {8, 0, 9};
-        long[] factorials3 = calculateFactorial(numbers3);
-        printFactorial(numbers3, factorials3);
+        numbers = new int[]{8, 0, 9};
+        factorials = calculateFactorial(numbers);
+        printFactorialsExpr(numbers, factorials);
 
-        int[] numbers4 = {-3, 1, 7, 13};
-        long[] factorials4 = calculateFactorial(numbers4);
-        printFactorial(numbers4, factorials4);
+        numbers = new int[]{-3, 1, 7, 13};
+        factorials = calculateFactorial(numbers);
+        printFactorialsExpr(numbers, factorials);
 
-        int[] numbers5 = {-22, -0};
-        long[] factorials5 = calculateFactorial(numbers5);
-        printFactorial(numbers5, factorials5);
+        numbers = new int[]{-22, -0};
+        factorials = calculateFactorial(numbers);
+        printFactorialsExpr(numbers, factorials);
     }
 
     private static long[] calculateFactorial(int... numbers) {
@@ -43,13 +43,13 @@ public class FactorialCalculator {
         return factorials;
     }
 
-    private static void printFactorial(int[] numbers, long... factorials) {
+    private static void printFactorialsExpr(int[] numbers, long[] factorials) {
         if (numbers == null) {
-            System.out.println("Ошибка ввода данных, расчет факториала невозможен\n");
+            System.out.println("Ошибка: массив не инициализирован\n");
             return;
         }
         if (numbers.length == 0) {
-            System.out.println("Переданный массив не содержит данных, расчет факториала не возможен\n");
+            System.out.println("Массив пуст. Нет данных для вывода факториалов.\n");
             return;
         }
 
@@ -61,11 +61,12 @@ public class FactorialCalculator {
             if (numbers[i] == 0 || numbers[i] == 1) {
                 System.out.println(numbers[i] + "! = " + factorials[i]);
             } else {
-                System.out.print(numbers[i] + "! = ");
+                StringBuilder expression = new StringBuilder(numbers[i] + "! = ");
                 for (int j = 1; j <= numbers[i]; j++) {
-                    System.out.printf("%d%s", j, j != numbers[i] ? " * " : "");
+                    expression.append(String.format("%d%s", j, j != numbers[i] ? " * " : ""));
                 }
-                System.out.println(" = " + factorials[i]);
+                expression.append(" = ").append(factorials[i]);
+                System.out.println(expression);
             }
         }
         System.out.println();
