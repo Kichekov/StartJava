0a9ecdac0ae039d6100482cf1e1ebc18c742cffc
Kichekov Vlad <kichekov@inbox.lv.>
2025-05-30 22:59:53 +0300

    Lesson 4. UniqueArraySorter var2

--- src/com/startjava/lesson_2_3_4/array/UniqueArraySorter.java
+++ src/com/startjava/lesson_2_3_4/array/UniqueArraySorter.java
@@ -6,29 +6,23 @@ import java.util.Random;
 public class UniqueArraySorter {
     public static void main(String[] args) {
         int[] numbers = generateRandomArray(-30, -10, 23);
-        sortArray(numbers);
         printArray(numbers, 23);
 
         numbers = generateRandomArray(10, 50, 10);
-        sortArray(numbers);
         printArray(numbers, 10);
 
         numbers = generateRandomArray(-34, -34, 0);
-        sortArray(numbers);
         printArray(numbers, 0);
 
         numbers = generateRandomArray(-1, 2, -3);
-        sortArray(numbers);
         printArray(numbers, -3);
 
         numbers = generateRandomArray(5, -8, 2);
-        sortArray(numbers);
         printArray(numbers, 2);
     }
 
     private static int[] generateRandomArray(int startRange, int endRange, int itemsPerLine) {
         int range = endRange - startRange + 1;
-        int size = (int) (range * 0.75);
 
         if (startRange > endRange) {
             System.out.printf("Ошибка: левая граница (%d) > правой (%d)\n\n", startRange, endRange);
@@ -40,6 +34,7 @@ public class UniqueArraySorter {
             return null;
         }
 
+        int size = (int) (range * 0.75);
         if (size <= 0) {
             System.out.printf("Ошибка: длина массива должна быть > 0 (%d)\n", size);
             return null;
@@ -49,40 +44,21 @@ public class UniqueArraySorter {
         int[] uniqueNumbers = new int[size];
         int uniqueCount = 0;
         while (uniqueCount < size) {
-            int number = startRange + random.nextInt(range);
-            boolean isFound = false;
+            int currNumber = startRange + random.nextInt(range);
+            boolean isUnique = false;
 
             for (int j = 0; j < uniqueCount; j++) {
-                if (uniqueNumbers[j] == number) {
-                    isFound = true;
+                if (uniqueNumbers[j] == currNumber) {
+                    isUnique = true;
                     break;
                 }
             }
-            if (!isFound) {
-                uniqueNumbers[uniqueCount++] = number;
+            if (!isUnique) {
+                uniqueNumbers[uniqueCount++] = currNumber;
             }
         }
+        Arrays.sort(uniqueNumbers);
         return Arrays.copyOf(uniqueNumbers, uniqueCount);
-    }
-
-    private static void sortArray(int[] numbers) {
-        if (numbers == null) {
-            return;
-        }
-        for (int i = 0; i < numbers.length; i++) {
-            boolean isSwapped = false;
-            for (int j = 0; j < numbers.length - 1; j++) {
-                if (numbers[j] > numbers[j + 1]) {
-                    isSwapped = true;
-                    int temp = numbers[j];
-                    numbers[j] = numbers[j + 1];
-                    numbers[j + 1] = temp;
-                }
-            }
-            if (!isSwapped) {
-                break;
-            }
-        }
     }
 
     private static void printArray(int[] numbers, int itemsPerLine) {
